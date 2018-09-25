679502a3610bacee0e9e7086600748da691ca8b0
Dmitrii Misharov <dmisharo@redhat.com>
2018-09-25 12:46:59 +0200

    Added City field

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,7 @@ from app.models import User
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
+    city = StringField(_l('City'))
     about_me = TextAreaField(_l('About me'),
                              validators=[Length(min=0, max=140)])
     submit = SubmitField(_l('Submit'))
